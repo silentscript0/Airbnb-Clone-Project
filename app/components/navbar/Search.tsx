1c723283b2ccc5a73f4c3a06ec792f64fc73cbd5
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2024-04-02 16:20:10 -0500

    added usermenu,avatar components and responsiveness

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { BiSearch } from 'react-icons/bi';
+
 
 const Search = () => {
     return(
@@ -22,6 +24,38 @@ const Search = () => {
                 <div className="text-sm font-semibold px-6">
                     Anywhere
                 </div>
+                    <div className="
+                        hidden
+                        sm:block
+                        text-sm
+                        font-semibold
+                        px-6
+                        border-x-[1px]
+                        flex-1
+                        text-center
+                    ">
+                        Any Week
+                    </div>
+                    <div className="
+                        text-sm
+                        pl-6
+                        pr-2
+                        text-gray-600
+                        flex
+                        flex-row
+                        items-center
+                        gap-3  
+                    ">
+                        <div className="hidden sm:block">Add Guests</div>
+                        <div className="
+                            p-2
+                            bg-rose-500
+                            rounded-full
+                            text-white
+                        ">
+                            <BiSearch size={18} />
+                        </div>
+                    </div>
             </div>
        </div> 
     );
