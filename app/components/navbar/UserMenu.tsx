7eb996c1ad6830882fd6c2da59212dc649e3c747
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2024-04-02 19:30:25 -0500

    added login and sign up menuitems and functions

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,8 +1,15 @@
 'use client';
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
+import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, []);
+
     return(
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
@@ -20,7 +27,7 @@ const UserMenu = () => {
                     ">
                     Airbnb your home
                 </div>
-                <div onClick={() => {}} className="
+                <div onClick={toggleOpen} className="
                  p-4
                  md:py-1 
                  md:px-2 
@@ -41,8 +48,33 @@ const UserMenu = () => {
                 </div>
                 </div>
             </div>
+            {isOpen && (
+                <div className="
+                 absolute
+                 rounded-xl
+                 shadow-md
+                 w-[40vw]
+                 md:w-3/4
+                 bg-white
+                 overflow-hidden
+                 right-0
+                 top-12
+                 text-sm
+                ">
+                    <div className="flex flex-col cursor-pointer">
+                        <>
+                        <MenuItem onClick={() => {}}
+                         label="Login"/> 
+
+                         <MenuItem onClick={() => {}}
+                         label="Sign up"/> 
+                        </>
+                    </div>
+
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
 export default UserMenu;
