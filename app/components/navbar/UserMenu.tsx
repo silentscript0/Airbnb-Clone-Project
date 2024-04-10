4bcfb37f837d674114e7837853645bd1c5af2cfb
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2024-04-10 18:11:31 -0500

    login with google and github

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -5,12 +5,12 @@ import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
-import { User } from '@prisma/client';
+import { SafeUser } from '@/app/types';
 import { signOut } from 'next-auth/react';
 import { useEffect } from 'react';
 
 interface UserMenuProps {
-    currentUser?: User | null
+    currentUser?: SafeUser | null
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({
@@ -58,7 +58,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                  ">
                 <AiOutlineMenu />
                 <div className="hidden md:block ">
-                    <Avatar />
+                    <Avatar src={currentUser?.image}/>
                 </div>
                 </div>
             </div>
