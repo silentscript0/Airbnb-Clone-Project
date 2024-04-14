62c05c439dd08829a5668bbb9d9cac52ababaf94
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2024-04-14 10:19:55 -0500

    image upload cloudinary

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -30,6 +30,7 @@ const RentModal = ()=> {
     const rentModal = useRentModal();
     const [step, setStep] = useState(STEPS.CATEGORY);
 
+
     const {
         register,
         handleSubmit,
@@ -120,6 +121,72 @@ const RentModal = ()=> {
             <Heading title="Where is your place located?" subtitle="Help guests find you" />
             <CountrySelect onChange={(value) => setCustomValue('location', value)} />
             <Map center={location?.latlng} />
+          </div>
+        );
+      }
+
+      if (step === STEPS.INFO) {
+        bodyContent = (
+          <div className="flex flex-col gap-8">
+            <Heading title="Share some basics about your place" subtitle="What amenities do you have?" />
+            <Counter
+              title="Guests"
+              subtitle="How many guests do you allow?"
+              value={guestCount}
+              onChange={(value) => setCustomValue('guestCount', value)}
+            />
+            <hr />
+            <Counter
+              title="Rooms"
+              subtitle="How many rooms do you have?"
+              value={roomCount}
+              onChange={(value) => setCustomValue('roomCount', value)}
+            />
+            <hr />
+            <Counter
+              title="Bathrooms"
+              subtitle="How many bathrooms do you have?"
+              value={bathroomCount}
+              onChange={(value) => setCustomValue('bathroomCount', value)}
+            />
+          </div>
+        );
+      }
+
+      if (step === STEPS.IMAGES) {
+        bodyContent = (
+          <div className="flex flex-col gap-8">
+            <Heading title="Add a photo of your place" subtitle="Show guests what your place looks like!" />
+            <ImageUpload value={imageSrc} onChange={(value) => setCustomValue('imageSrc', value)} />
+          </div>
+        );
+      }
+    
+      if (step === STEPS.DESCRIPTION) {
+        bodyContent = (
+          <div className="flex flex-col gap-8">
+            <Heading title="How would you describe your place?" subtitle="Short and sweet works best" />
+            <Input id="title" label="Title" disabled={isLoading} register={register} errors={errors} required />
+            <hr />
+            <Input id="description" label="Description" disabled={isLoading} register={register} errors={errors} required />
+          </div>
+        );
+      }
+
+      if (step === STEPS.PRICE) {
+        bodyContent = (
+          <div className="flex flex-col gap-8">
+            <Heading title="Now, set your price" subtitle="How much do you charge per night?" />
+            <Input
+              id="price"
+              label="Price"
+              formatPrice
+              type="number"
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
           </div>
         );
       }
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     experimental: {
         appDir: true,
-    },  images: { domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"]} 
+    },  images: { domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com", "res.cloudinary.com"]} 
 };
 
 module.exports = nextConfig;
