c1571dc9d5f44b257f293ed3e42d0e81da13bd73
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2024-04-15 14:28:26 -0500

    fetch listing with server componetns and favorite rentals

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+import getCurrentUser from './actions/getCurrentUser';
+import getListings, { IListingsParams } from './actions/getListings';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
+import { SafeListing } from './types';
+export const dynamic = 'force-dynamic';
 
-
-export default function Home() {
-  return(
-    <div className="text-rose-500 text-2xl">Hello Airbnb</div>
-  )
+interface HomeProps {
+  searchParams: IListingsParams;
 }
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  const isEmpty = listings.length === 0;
+
+  if (isEmpty) {
+    return <EmptyState showReset />;
+  }
+
+  return (
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: SafeListing) => {
+          return <ListingCard currentUser={currentUser} key={listing.id} data={listing} />;
+        })}
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,12 +1,16 @@
-import { User } from "@prisma/client";
+import { Listing, Reservation, User } from '@prisma/client';
 
-export type SafeUser = Omit<
- User,
- "createdAt" | "updatedAt" | "emailVerified"
- > & { 
-    createdAt: string;
-    updatedAt: string;
-    emailVerified: string | null;
- };
+export type SafeListing = Omit<Listing, 'createdAt'> & { createdAt: string };
 
- 
+export type SafeReservation = Omit<Reservation, 'createdAt' | 'startDate' | 'endDate' | 'listing'> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string | null;
+  listing: SafeListing;
+};
+
+export type SafeUser = Omit<User, 'createdAt' | 'updatedAt' | 'emailVerified'> & {
+  createdAt: string;
+  updatedAt: string;
+  emailVerified: string | null;
+};
