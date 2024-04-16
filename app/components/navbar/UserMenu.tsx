4a1ff2fd76374521156efdd9c3c26985e1006516
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2024-04-16 13:53:38 -0500

    added favorite trips component

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -93,7 +93,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                              label="My trips"/> 
     
                              <MenuItem 
-                              onClick={() => {}}
+                              onClick={() => router.push('/favorites')}
                               label="My favorites"
                              /> 
                              <MenuItem 
