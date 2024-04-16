b0676747c27cfd78f6d889348a929aae42377142
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2024-04-16 13:27:20 -0500

    added my reservations component

---
+++ app/reservations/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import getCurrentUser from '../actions/getCurrentUser';
+import EmptyState from '../components/EmptyState';
+import getReservations from '../actions/getReservations';
+import ReservationsClient from './ReservationsClient';
+import ClientOnly from '../components/ClientOnly';
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return(
+        <ClientOnly>
+            <EmptyState title="Unauthorized" subtitle="Please login" />;
+        </ClientOnly>
+    ) 
+  }
+
+  const reservations = await getReservations({ authorId: currentUser?.id });
+
+  if (reservations.length === 0) {
+    return (
+        <ClientOnly>
+            <EmptyState title="No reservations found" subtitle="Looks like you have no reservations on your properties" />
+       </ClientOnly>
+
+    );
+      
+  }
+  return <ReservationsClient reservations={reservations} currentUser={currentUser} />;
+};
+
+export default ReservationsPage;
